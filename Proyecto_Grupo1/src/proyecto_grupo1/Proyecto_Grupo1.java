2ea831fe945d29711a657e186cabea86c374b966
joSEph3112Da <josephalvaradog3112@gmail.com>
2024-11-05 20:21:18 -0600

    Inicio del proyecto

---
+++ Proyecto_Grupo1/src/proyecto_grupo1/Proyecto_Grupo1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_grupo1;
+
+/**
+ *
+ * @author Joseph
+ */
+public class Proyecto_Grupo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
