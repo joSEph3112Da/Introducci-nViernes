2da4d4fa51e046e56e3935c32383e60b16eb7165
joSEph3112Da <josephalvaradog3112@gmail.com>
2024-11-05 20:32:34 -0600

    Modificación Main

---
+++ Proyecto_Grupo1/src/proyecto_grupo1/Estado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package proyecto_grupo1;
+
+/**
+ *
+ * @author Joseph
+ */
+public enum Estado {
+    Activo, Inactivo;
+
+}
---
+++ Proyecto_Grupo1/src/proyecto_grupo1/Quickpass.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_grupo1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Joseph
+ */
+public class Quickpass {
+    String filial;
+    int codigo;
+    int placa;
+    Estado estado;
+    
+    
+    public Quickpass(String pFilial, String pCodigo,int pPlaca){
+        this.filial = pFilial;
+        this.codigo = setCodigo(pCodigo);
+        this.placa = pPlaca;
+        this.estado = Estado.Activo;
+    }
+
+    public int setCodigo(String pCodigo) {
+        
+        if (pCodigo.length() == 10) {
+            if (pCodigo.substring(0,3).equals("101")) {
+                return Integer.parseInt(pCodigo);
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "El código debe comenzar con 101","Error",JOptionPane.ERROR_MESSAGE);
+                return -2;
+            }
+            
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "El código debe tener 10 dígitos","Error",JOptionPane.ERROR_MESSAGE);
+            return -1;
+        }
+           
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return "Quickpass [filial=" + filial + ", codigo=" + codigo + ", placa=" + placa + ", estado=" + estado + "]";
+    }
+}
